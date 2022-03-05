b4195fd3c457c394f20690c579a208e106e27d4e
Animesh Singh <66486050+animeshdebug7@users.noreply.github.com>
2022-03-05 12:56:54 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -45,9 +45,9 @@ class Chatbox {
 
         let msg1 = { name: "User", message: text1 }
         this.messages.push(msg1);
-//         'http://127.0.0.1:5000/predict'
-//         fetch($SCRIPT_ROOT +'/predict',
-        fetch('/predict', {
+        'http://127.0.0.1:5000/predict'
+        fetch($SCRIPT_ROOT +'/predict', {
+//         fetch('/predict', {
             method: 'POST',
             body: JSON.stringify({ message: text1 }),
             mode: 'cors',
